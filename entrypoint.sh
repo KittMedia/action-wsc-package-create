03de405592283cd5e1b2ad235908a49326e31cf9
Matthias Kittsteiner <mk+github@kitt.media>
2020-11-23 11:22:36 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,13 +2,10 @@
 # Build a valid WCF package
 
 PACKAGE_NAME=${GITHUB_REPOSITORY#*/}
-#OPTIONAL_DIR=optional
 
 [ -z $PACKAGE_NAME ] && exit 1
-cd ..
-[ ! -z $OPTIONAL_DIR ] && [ ! -d $OPTIONAL_DIR ] && exit 1
-[ ! -z $OPTIONAL_DIR ] && cd $OPTIONAL_DIR
 cd $GITHUB_WORKSPACE
+ls -l
 test -e composer.json && echo -e "\nComposer installation\n-----------" && composer install
 test -e acptemplate && echo -e "\nBuilding acptemplate.tar\n-------------------------" && cd acptemplate && tar cvf ../acptemplate.tar --exclude=.git* * && cd ..
 test -e acptemplates && echo -e "\nBuilding acptemplates.tar\n-------------------------" && cd acptemplates && tar cvf ../acptemplates.tar --exclude=.git* * && cd ..
@@ -27,16 +24,5 @@ do
 done
 echo -en "\n"
 tar cvfz $PACKAGE_NAME.tar.gz --exclude=file --exclude=files --exclude=files_preinstall --exclude=files_wcf --exclude=acptemplate --exclude=acptemplates --exclude=template --exclude=templates --exclude=wcf-buildscripts --exclude=README* --exclude=CHANGELOG --exclude=LICENSE --exclude=.git* --exclude=composer* *
-
-[ ! -z $OPTIONAL_DIR ] && mv $PACKAGE_NAME.tar.gz .. && cd ..
-
-test -e acptemplate.tar && rm acptemplate.tar
-test -e acptemplates.tar && rm acptemplates.tar
-test -e file.tar && rm file.tar
-test -e files.tar && rm files.tar
-test -e files_preinstall.tar && rm files_preinstall.tar
-test -e files_wcf.tar && rm files_wcf.tar
-test -e template.tar && rm template.tar
-test -e templates.tar && rm templates.tar
 
 exit 0
