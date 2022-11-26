c9b9b7553af05e0ded89ee238980192d7401b2c3
Matthias Kittsteiner <mk@kittmedia.com>
2022-11-26 21:55:53 +0100

    Remove asterisk for files_preinstall

--- entrypoint.sh
+++ entrypoint.sh
@@ -44,7 +44,7 @@ test -e acptemplate && echo -e "\nBuilding acptemplate.tar\n--------------------
 test -e acptemplates && echo -e "\nBuilding acptemplates.tar\n-------------------------" && cd acptemplates && tar cvf ../acptemplates.tar --exclude=.git* * && cd ..
 test -e file && echo -e "\nBuilding file.tar\n------------------" && cd file && tar cvf ../file.tar $TAR_EXCLUDES_FILES * && cd ..
 test -e files && echo -e "\nBuilding files.tar\n------------------" && cd files && tar cvf ../files.tar --exclude=.git* $TAR_EXCLUDES_FILES * && cd ..
-test -e files_preinstall && echo -e "\nBuilding files_preinstall.tar\n-----------------------------" && cd files_preinstall && tar cvf ../files_preinstall.tar $TAR_EXCLUDES_FILES* * && cd ..
+test -e files_preinstall && echo -e "\nBuilding files_preinstall.tar\n-----------------------------" && cd files_preinstall && tar cvf ../files_preinstall.tar $TAR_EXCLUDES_FILES * && cd ..
 test -e files_wcf && echo -e "\nBuilding files_wcf.tar\n----------------------" && cd files_wcf && tar cvf ../files_wcf.tar $TAR_EXCLUDES_FILES * && cd ..
 test -e template && echo -e "\nBuilding template.tar\n----------------------" && cd template && tar cvf ../template.tar --exclude=.git* * && cd ..
 test -e templates && echo -e "\nBuilding templates.tar\n----------------------" && cd templates && tar cvf ../templates.tar --exclude=.git* * && cd ..
